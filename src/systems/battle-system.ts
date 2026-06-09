aaa8f99d9a3067d19b1134e1d66fad6678d72c13
behassan <behassan@deloitte.co.il>
2026-06-09 21:03:32 +0300

    fix

--- src/systems/battle-system.ts
+++ src/systems/battle-system.ts
@@ -24,6 +24,7 @@ import {
   getPokemonDisplayName,
 } from '../services/pokemon-data.js';
 import { getItem } from '../data/items.js';
+import { t } from '../i18n/i18n.js';
 
 export interface TurnOrderDecision {
   enemyActsFirst: boolean;
